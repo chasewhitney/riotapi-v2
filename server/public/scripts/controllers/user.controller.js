7e8ad3a46bc66559211eac6e18e766e4c98a3ab1
Chase <ChaseEWhitney@gmail.com>
2018-05-18 12:08:53 -0500

    sunburst object successfully building from generic data

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -4,6 +4,46 @@ myApp.controller('UserController', function(UserService, $http, $location, $mdDi
   vm.userService = UserService;
   vm.progressVal = 0;
 
+  var gDat = [
+    {champ: "Shaco", lane : "Jungle", win: true},
+    {champ: "Shaco", lane : "Jungle", win: false},
+    {champ: "Shaco", lane : "Jungle", win: true},
+    {champ: "Teemo", lane : "Jungle", win: false},
+    {champ: "Ahri", lane : "Middle", win: true},
+    {champ: "Teemo", lane : "Top", win: true},
+    {champ: "Brand", lane : "Middle", win: false},
+    {champ: "Ezreal", lane : "Bottom", win: false},
+    {champ: "Shaco", lane : "Jungle", win: true},
+    {champ: "Teemo", lane : "Middle", win: false},
+  ]
+  //////////////// TESTING
+  function treeObj(name){
+    this.name = name;
+    this.children = [];
+  }
+
+  function buildObj(){
+    var finalObj = new treeObj("Lane");
+
+    gDat.forEach((gv,gi)=>{
+      if(!finalObj.children[gv.lane]){
+        finalObj.children[gv.lane] = new treeObj(gv.lane);
+      }
+      if(!finalObj.children[gv.lane].children[gv.champ]){
+        finalObj.children[gv.lane].children[gv.champ] = new treeObj(gv.champ);
+        finalObj.children[gv.lane].children[gv.champ].children['Wins'] = {name: 'Wins', size: 0, children: []};
+        finalObj.children[gv.lane].children[gv.champ].children['Losses'] = {name: 'Losses', size: 0, children: []};
+      }
+      if(gv.win){
+        finalObj.children[gv.lane].children[gv.champ].children['Wins'].size +=1;
+      } else {
+        finalObj.children[gv.lane].children[gv.champ].children['Losses'].size +=1;
+      }
+    });
+    console.log(finalObj);
+  }
+  buildObj();
+  ///////////////////  END TESTING
   vm.doAll = function(name) {
     getPlayerAccount(name)
     .then(getMatches)
@@ -62,17 +102,17 @@ myApp.controller('UserController', function(UserService, $http, $location, $mdDi
 
   function doThing(result) {
     console.log(`result is:`, result);
-
+    console.log('------------');
 
     result.forEach((v,i)=>{
       var pMD = v.participants[getParticipantIndex(v)]; // player match data
-      //pMD.timeline.lane
-      console.log('------------');
-      console.log(pMD.timeline.lane);
-      //pMd.championId
+
       console.log(pMD.championId);
-      //pMD.stats.win
+
+      console.log(pMD.timeline.lane); //NONE if game ends before 20
+
       console.log(pMD.timeline.role);
+      console.log((v.gameDuration / 60) + ' min');
       console.log('------------');
     })
 
